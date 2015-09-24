e05e339f37f834969b86e33b4510d6941ed79763
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-09-24 00:17:24 -0400

    Add renewals over time chart

---
+++ lib/Admin/Reports/Types/Renewals_Over_Time.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Renewals over time report.
+ *
+ * @author Iron Bound Designs
+ * @since  1.0
+ */
+
+namespace ITELIC\Admin\Reports\Types;
+
+use IronBound\DB\Manager;
+use ITELIC\Admin\Chart\Base;
+use ITELIC\Admin\Chart\Line;
+use ITELIC\Admin\Reports\Report;
+use ITELIC\Plugin;
+
+/**
+ * Class Renewals_Over_Time
+ * @package ITELIC\Admin\Reports\Types
+ */
+class Renewals_Over_Time extends Report {
+
+	/**
+	 * Get the title of this report type.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		return __( "Renewals over time", Plugin::SLUG );
+	}
+
+	/**
+	 * Get the slug of this report type.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string
+	 */
+	public function get_slug() {
+		return 'renewals-over-time';
+	}
+
+	/**
+	 * Get the description of this report type.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string
+	 */
+	public function get_description() {
+		return __(
+			"Chart renewals for all products, or a specific product, over time.",
+			Plugin::SLUG
+		);
+	}
+
+	/**
+	 * Get the chart for this report.
+	 *
+	 * @since 1.0
+	 *
+	 * @param string $date_type
+	 * @param int    $product
+	 *
+	 * @return Base
+	 */
+	public function get_chart( $date_type = 'this_year', $product = 0 ) {
+
+		$start = date( 'Y-m-d H:i:s', $this->convert_date( $date_type ) );
+		$end   = date( 'Y-m-d H:i:s', $this->convert_date( $date_type, true ) );
+
+		$grouping = self::get_grouping_for_date_type( $date_type );
+
+		$sql   = self::get_group_by( $grouping, 'r.renewal_date' );
+		$group = $sql['group'];
+		$per   = $sql['per'];
+
+		if ( $per ) {
+			$per .= ' AS d, ';
+		}
+
+		if ( $group ) {
+			$group = "GROUP BY $group";
+		}
+
+		/**
+		 * @var \wpdb $wpdb
+		 */
+		global $wpdb;
+
+		$rtn = Manager::get( 'itelic-renewals' )->get_table_name( $wpdb );
+		$ktn = Manager::get( 'itelic-keys' )->get_table_name( $wpdb );
+
+		if ( $product ) {
+			$raw = "SELECT {$per}COUNT(1) as c FROM $rtn r JOIN $ktn k ON(k.lkey = r.lkey AND k.product = %d) WHERE r.renewal_date BETWEEN %s AND %s";
+			$raw .= $group;
+			$renewals = $wpdb->get_results( $wpdb->prepare( $raw, $product, $start, $end ) );
+		} else {
+			$raw = "SELECT {$per}COUNT(1) as c FROM $rtn r  WHERE r.renewal_date BETWEEN %s AND %s";
+			$raw .= $group;
+			$renewals = $wpdb->get_results( $wpdb->prepare( $raw, $start, $end ) );
+		}
+
+		$renewals = self::fill_gaps( self::translate_results( $renewals ), $start, $end, $grouping );
+
+		$labels = self::get_labels( $renewals, $date_type );
+
+		$chart = new Line( $labels, 600, 200, array(
+			//'ibdShowLegend' => '#legend-' . $this->get_slug(),
+			'responsive' => true
+		) );
+
+		$chart->add_data_set( array_values( $renewals ), __( "Renewals", Plugin::SLUG ), array(
+			'fillColor'            => "rgba(151,187,205,0.2)",
+			'strokeColor'          => "rgba(151,187,205,1)",
+			'pointColor'           => "rgba(151,187,205,1)",
+			'pointStrokeColor'     => "#fff",
+			'pointHighlightFill'   => "#fff",
+			'pointHighlightStroke' => "rgba(151,187,205,1)",
+		) );
+
+		return $chart;
+	}
+}
--- lib/Admin/Reports/load.php
+++ lib/Admin/Reports/load.php
@@ -13,6 +13,7 @@ use ITELIC\Admin\Reports\Controller\SingleC;
 use ITELIC\Admin\Reports\Types\Installed_Versions;
 use ITELIC\Admin\Reports\Types\Licenses;
 use ITELIC\Admin\Reports\Types\Renewal_Rate;
+use ITELIC\Admin\Reports\Types\Renewals_Over_Time;
 
 Dispatch::register_view( 'list', new ListC() );
 Dispatch::register_view( 'single', new SingleC() );
@@ -20,3 +21,4 @@ Dispatch::register_view( 'single', new SingleC() );
 Dispatch::register_report( new Licenses() );
 Dispatch::register_report( new Installed_Versions() );
 Dispatch::register_report( new Renewal_Rate() );
+Dispatch::register_report( new Renewals_Over_Time() );
