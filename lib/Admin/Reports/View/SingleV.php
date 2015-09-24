e587d1aff382e4582c1f0bb8871b17661b40522f
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-09-23 21:43:58 -0400

    Allow for changing the report interval

--- lib/Admin/Reports/View/SingleV.php
+++ lib/Admin/Reports/View/SingleV.php
@@ -42,7 +42,32 @@ class SingleV extends View {
 		if ( ! $this->report ) {
 			return;
 		}
+
+		$selected_type = isset( $_GET['date_type'] ) ? $_GET['date_type'] : 'this_year';
 		?>
+
+		<form method="GET" class="filter-form">
+
+			<input type="hidden" name="page" value="<?php echo $_GET['page']; ?>">
+			<input type="hidden" name="tab" value="<?php echo $_GET['tab']; ?>">
+			<input type="hidden" name="view" value="<?php echo $_GET['view']; ?>">
+			<input type="hidden" name="report" value="<?php echo $_GET['report']; ?>">
+
+			<label for="date-type" class="screen-reader-text"><?php _e( "Select a date range.", Plugin::SLUG ); ?></label>
+			<select name="date_type" id="date-type">
+
+				<?php foreach ( $this->report->get_date_types() as $type => $label ): ?>
+
+					<option value="<?php echo $type; ?>" <?php selected( $type, $selected_type ) ?>>
+						<?php echo $label; ?>
+					</option>
+
+				<?php endforeach; ?>
+			</select>
+
+			<?php submit_button( __( "Filter", Plugin::SLUG ), 'button', 'submit', false ); ?>
+
+		</form>
 
 		<div class="report report-<?php echo $this->report->get_slug(); ?>">
 
@@ -53,7 +78,7 @@ class SingleV extends View {
 			</p>
 
 			<div class="chart">
-				<?php $this->report->get_chart()->graph(); ?>
+				<?php $this->report->get_chart( $selected_type )->graph(); ?>
 			</div>
 
 			<div id="legend-<?php echo $this->report->get_slug(); ?>" class="chart-js-legend"></div>
