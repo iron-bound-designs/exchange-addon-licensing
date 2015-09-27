32e2a8a378b96313895e33125be69f6086315820
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-09-27 11:41:29 -0400

    Add activation meta table

---
+++ lib/DB/Table/Activation_Meta.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Activation Meta Table
+ *
+ * @author      Iron Bound Designs
+ * @since       1.0
+ * @copyright   2015 (c) Iron Bound Designs.
+ * @license     GPLv2
+ */
+
+namespace ITELIC\DB\Table;
+
+use IronBound\DB\Table\Table;
+
+/**
+ * Class Activation_Meta
+ *
+ * @package ITELIC\DB\Table
+ */
+class Activation_Meta implements Table {
+
+	/**
+	 * Retrieve the name of the database table.
+	 *
+	 * @since 1.0
+	 *
+	 * @param \wpdb $wpdb
+	 *
+	 * @return string
+	 */
+	public function get_table_name( \wpdb $wpdb ) {
+		return $wpdb->prefix . 'itelic_activationmeta';
+	}
+
+	/**
+	 * Get the slug of this table.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string
+	 */
+	public function get_slug() {
+		return 'itelic-activation-meta';
+	}
+
+	/**
+	 * Columns in the table.
+	 *
+	 * key => sprintf field type
+	 *
+	 * @since 1.0
+	 *
+	 * @return array
+	 */
+	public function get_columns() {
+		return array(
+			'meta_id'       => '%d',
+			'activation_id' => '%d',
+			'meta_key'      => '%s',
+			'meta_value'    => '%s'
+		);
+	}
+
+	/**
+	 * Default column values.
+	 *
+	 * @since 1.0
+	 *
+	 * @return array
+	 */
+	public function get_column_defaults() {
+		return array(
+			'meta_id'       => 0,
+			'activation_id' => 0,
+			'meta_key'      => '',
+			'meta_value'    => ''
+		);
+	}
+
+	/**
+	 * Retrieve the name of the primary key.
+	 *
+	 * @since 1.0
+	 *
+	 * @return string
+	 */
+	public function get_primary_key() {
+		return 'meta_id';
+	}
+
+	/**
+	 * Get creation SQL.
+	 *
+	 * @since 1.0
+	 *
+	 * @param \wpdb $wpdb
+	 *
+	 * @return string
+	 */
+	public function get_creation_sql( \wpdb $wpdb ) {
+
+		$tn = $this->get_table_name( $wpdb );
+
+		return "CREATE TABLE {$tn} (
+		meta_id bigint(20) unsigned NOT NULL auto_increment,
+		activation_id bigint(20) unsigned NOT NULL,
+		meta_key varchar(255) NOT NULL,
+		meta_value longtext,
+		PRIMARY KEY  (meta_id),
+		KEY activation_id (activation_id),
+		KEY meta_key (meta_key(191))
+		) {$wpdb->get_charset_collate()};";
+	}
+
+	/**
+	 * Retrieve the version number of the current table schema as written.
+	 *
+	 * The version should be incremented by 1 for each change.
+	 *
+	 * @since 1.0
+	 *
+	 * @return int
+	 */
+	public function get_version() {
+		return 1;
+	}
+
+}
--- lib/DB/load.php
+++ lib/DB/load.php
@@ -9,6 +9,7 @@
 namespace ITELIC\DB;
 
 use IronBound\DB\Manager;
+use ITELIC\DB\Table\Activation_Meta;
 use ITELIC\DB\Table\Keys;
 use ITELIC\DB\Table\Activations;
 use ITELIC\DB\Table\Release_Meta;
@@ -22,15 +23,21 @@ Manager::register( new Renewals() );
 Manager::register( new Releases() );
 Manager::register( new Updates() );
 Manager::register( new Release_Meta() );
+Manager::register( new Activation_Meta() );
 
 global $wpdb;
 
-$wpdb->itelic_releasemeta = Manager::get( 'itelic-release-meta' )->get_table_name( $wpdb );
+$wpdb->itelic_releasemeta    = Manager::get( 'itelic-release-meta' )->get_table_name( $wpdb );
+$wpdb->itelic_activationmeta = Manager::get( 'itelic-activation-meta' )->get_table_name( $wpdb );
 
 add_filter( 'sanitize_key', function ( $sanitized, $original ) {
 
 	if ( $original == 'itelic_release_id' ) {
 		$sanitized = sanitize_key( 'release_id' );
+	}
+
+	if ( $original == 'itelic_activation_id' ) {
+		$sanitized = sanitize_key( 'activation_id' );
 	}
 
 	return $sanitized;
@@ -50,6 +57,7 @@ function itelic_install_tables() {
 	Manager::maybe_install_table( new Releases() );
 	Manager::maybe_install_table( new Updates() );
 	Manager::maybe_install_table( new Release_Meta() );
+	Manager::maybe_install_table( new Activation_Meta() );
 }
 
 add_action( 'itelic_activate', __NAMESPACE__ . '\\itelic_install_tables' );
