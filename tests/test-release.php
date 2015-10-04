a99eb0adf6a5ef01bce53d748dbc6f04c17ccc07
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-10-03 21:32:52 -0400

    Add tests for activating releases

--- tests/test-release.php
+++ tests/test-release.php
@@ -171,4 +171,194 @@ class ITELIC_Test_Release extends ITELIC_UnitTestCase {
 
 		$this->assertEquals( wp_get_attachment_url( $file1 ), $download_data['source'] );
 	}
+
+	public function test_activating_release_updates_version() {
+
+		$product = $this->product_factory->create_and_get();
+
+		$file1 = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file1,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		$r->activate();
+
+		$saved_version = it_exchange_get_product_feature( $product->ID, 'licensing', array( 'field' => 'version' ) );
+
+		$this->assertEquals( '1.1', $saved_version );
+	}
+
+	public function test_activating_release_updates_url() {
+
+		$product = $this->product_factory->create_and_get( array(
+				'update-file' => wp_insert_post( array(
+					'post_type' => 'it_exchange_download'
+				) )
+			)
+		);
+
+		$file1 = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file1,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		$r->activate();
+
+		$download_id   = it_exchange_get_product_feature( $product->ID, 'licensing', array( 'field' => 'update-file' ) );
+		$download_data = get_post_meta( $download_id, '_it-exchange-download-info', true );
+
+		$this->assertEquals( wp_get_attachment_url( $file1 ), $download_data['source'] );
+	}
+
+	public function test_activating_release_updates_status() {
+
+		$product = $this->product_factory->create_and_get();
+		$file    = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		$r->activate();
+
+		$this->assertEquals( Release::STATUS_ACTIVE, $r->get_status() );
+	}
+
+	public function test_activating_release_sets_start_date() {
+
+		$product = $this->product_factory->create_and_get();
+		$file    = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		$r->activate();
+
+		$this->assertEquals( \ITELIC\make_date_time()->getTimestamp(), $r->get_start_date()->getTimestamp(), '', 5 );
+	}
+
+	public function test_activating_release_sets_start_date_with_custom_date() {
+
+		$product = $this->product_factory->create_and_get();
+		$file    = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		$r->activate( \ITELIC\make_date_time( 'yesterday' ) );
+
+		$this->assertEquals( \ITELIC\make_date_time( 'yesterday' )->getTimestamp(), $r->get_start_date()->getTimestamp(), '', 5 );
+	}
+
+	public function test_activating_pre_release_does_not_update_version() {
+
+		$product = $this->product_factory->create_and_get();
+
+		$file1 = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file1,
+			'version' => '1.1',
+			'type'    => Release::TYPE_PRERELEASE
+		) );
+
+		$r->activate();
+
+		$saved_version = it_exchange_get_product_feature( $product->ID, 'licensing', array( 'field' => 'version' ) );
+
+		$this->assertEquals( '1.0', $saved_version );
+	}
+
+	public function test_activating_pre_release_does_not_update_url() {
+
+		$product = $this->product_factory->create_and_get( array(
+				'update-file' => wp_insert_post( array(
+					'post_type' => 'it_exchange_download'
+				) )
+			)
+		);
+
+		$file1 = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file1,
+			'version' => '1.1',
+			'type'    => Release::TYPE_PRERELEASE
+		) );
+
+		$r->activate();
+
+		$download_id   = it_exchange_get_product_feature( $product->ID, 'licensing', array( 'field' => 'update-file' ) );
+		$download_data = get_post_meta( $download_id, '_it-exchange-download-info', true );
+
+		if ( ! is_array( $download_data ) ) {
+			$download_data['source'] = '';
+		}
+
+		$this->assertNotEquals( wp_get_attachment_url( $file1 ), $download_data['source'] );
+	}
+
+	public function test_activating_release_clears_changelog_cache() {
+
+		$product = $this->product_factory->create_and_get();
+		$file    = $this->factory->attachment->create_object( 'file.zip', $product->ID, array(
+			'post_mime_type' => 'application/zip'
+		) );
+
+		/** @var Release $r */
+		$r = $this->release_factory->create_and_get( array(
+			'product' => $product->ID,
+			'file'    => $file,
+			'version' => '1.1',
+			'type'    => Release::TYPE_MAJOR
+		) );
+
+		wp_cache_set( $r->get_product()->ID, 'test', 'itelic-changelog' );
+
+		$r->activate();
+
+		$this->assertEmpty( wp_cache_get( $r->get_product()->ID, 'itelic-changelog' ) );
+	}
 }
