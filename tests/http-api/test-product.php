f9321989d9eb6d6f05a10ddfb0729787c9ea6c17
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-10-07 12:31:21 -0400

    Add complete tests for the product endpoint

--- tests/http-api/test-product.php
+++ tests/http-api/test-product.php
@@ -52,15 +52,21 @@ class ITELIC_Test_HTTP_API_Product extends ITELIC_UnitTestCase {
 
 		$this->key        = $mock_key;
 		$this->activation = $mock_activation;
-	}
-
-	public function test_response_format() {
 
 		WP_Mock::wpFunction( 'ITELIC\generate_download_link', array(
 			'times'  => 1,
 			'args'   => array( $this->activation ),
 			'return' => 'www.example.com/download'
 		) );
+
+		WP_Mock::wpFunction( 'get_permalink', array(
+			'times'  => 1,
+			'args'   => array( 1 ),
+			'return' => 'www.example.com/product/1'
+		) );
+	}
+
+	public function test_response_format() {
 
 		$endpoint = new \ITELIC\API\Endpoint\Product();
 		$endpoint->set_auth_license_key( $this->key );
@@ -73,5 +79,154 @@ class ITELIC_Test_HTTP_API_Product extends ITELIC_UnitTestCase {
 		$this->assertInternalType( 'array', $data['body'], 'Response body is not an array.' );
 		$this->assertArrayHasKey( 'list', $data['body'], 'Response does not contain the product list.' );
 		$this->assertArrayHasKey( 1, $data['body']['list'], 'Product list does not contain the product from the license key.' );
+	}
+
+	public function test_name() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'Product Name', $data['body']['list'][1]['name'] );
+	}
+
+	public function test_description() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'This is the description.', $data['body']['list'][1]['description'] );
+	}
+
+	public function test_version() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( '1.2', $data['body']['list'][1]['version'] );
+	}
+
+	public function test_tested_upto() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( '4.4', $data['body']['list'][1]['tested'] );
+	}
+
+	public function test_requires() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( '4.3', $data['body']['list'][1]['requires'] );
+	}
+
+	public function test_contributors() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$contributors = array(
+			'User1' => '//profiles.wordpress.org/User1',
+			'User2' => '//profiles.wordpress.org/User2'
+		);
+
+		$this->assertEquals( $contributors, $data['body']['list'][1]['contributors'] );
+	}
+
+	public function test_last_updated() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( '2014-12-31T00:00:00+0000', $data['body']['list'][1]['last_updated'] );
+	}
+
+	public function test_banner_low() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'www.example.com/low', $data['body']['list'][1]['banner_low'] );
+	}
+
+	public function test_banner_high() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'www.example.com/high', $data['body']['list'][1]['banner_high'] );
+	}
+
+	public function test_package_url() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'www.example.com/download', $data['body']['list'][1]['package_url'] );
+	}
+
+	public function test_description_url() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'www.example.com/product/1', $data['body']['list'][1]['description_url'] );
+	}
+
+	public function test_changelog() {
+
+		$endpoint = new \ITELIC\API\Endpoint\Product();
+		$endpoint->set_auth_license_key( $this->key );
+		$endpoint->set_auth_activation( $this->activation );
+
+		$response = $endpoint->serve( new ArrayObject(), new ArrayObject() );
+		$data     = $response->get_data();
+
+		$this->assertEquals( 'Changes', $data['body']['list'][1]['changelog'] );
 	}
 }
