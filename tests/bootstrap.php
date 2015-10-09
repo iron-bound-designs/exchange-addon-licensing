f8abed6ad825664406a0626f89ab371c8a19f3ca
TimothyBJacobs <tim@tbjfilms.com>
2015-10-09 01:52:42 -0400

    When will this work

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,15 +6,32 @@
  * @since  1.0
  */
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+/**
+ * Determine where the WP test suite lives.
+ *
+ * Support for:
+ * 1. `WP_DEVELOP_DIR` environment variable, which points to a checkout
+ *   of the develop.svn.wordpress.org repository (this is recommended)
+ * 2. `WP_TESTS_DIR` environment variable, which points to a checkout
+ * 3. `WP_ROOT_DIR` environment variable, which points to a checkout
+ * 4. Plugin installed inside of WordPress.org developer checkout
+ * 5. Tests checked out to /tmp
+ */
+if ( false !== getenv( 'WP_DEVELOP_DIR' ) ) {
+	$test_root = getenv( 'WP_DEVELOP_DIR' ) . '/tests/phpunit';
+} else if ( false !== getenv( 'WP_TESTS_DIR' ) ) {
+	$test_root = getenv( 'WP_TESTS_DIR' );
+} else if ( false !== getenv( 'WP_ROOT_DIR' ) ) {
+	$test_root = getenv( 'WP_ROOT_DIR' ) . '/tests/phpunit';
+} else if ( file_exists( '../../../../tests/phpunit/includes/bootstrap.php' ) ) {
+	$test_root = '../../../../tests/phpunit';
+} else if ( file_exists( '/tmp/wordpress-tests-lib/includes/bootstrap.php' ) ) {
+	$test_root = '/tmp/wordpress-tests-lib';
 }
 
 define( 'DOING_TESTS', true );
 
-require_once $_tests_dir . '/includes/functions.php';
+require_once $test_root . '/includes/functions.php';
 
 require_once dirname( __FILE__ ) . '/../vendor/antecedent/patchwork/Patchwork.php';
 
@@ -34,7 +51,7 @@ function _manually_load_plugin() {
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
-require $_tests_dir . '/includes/bootstrap.php';
+require $test_root . '/includes/bootstrap.php';
 
 require dirname( __FILE__ ) . '/framework/shim.php';
 require dirname( __FILE__ ) . '/framework/product-factory.php';
