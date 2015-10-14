e6b1f97448477e385aa23573a17726483ec8db6b
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-10-13 20:31:20 -0400

    Make the changelog endpoint not require auth

--- lib/API/Endpoint/Changelog.php
+++ lib/API/Endpoint/Changelog.php
@@ -10,8 +10,6 @@
 
 namespace ITELIC\API\Endpoint;
 
-use ITELIC\Activation;
-use ITELIC\API\Contracts\Authenticatable;
 use ITELIC\API\Endpoint;
 use ITELIc\API\Exception;
 use ITELIC\API\Response;
@@ -22,12 +20,7 @@ use ITELIC\Plugin;
  * Class Changelog
  * @package ITELIC\API\Endpoint
  */
-class Changelog extends Endpoint implements Authenticatable {
-
-	/**
-	 * @var Key
-	 */
-	private $key;
+class Changelog extends Endpoint {
 
 	/**
 	 * Serve the request to this endpoint.
@@ -42,63 +35,15 @@ class Changelog extends Endpoint implements Authenticatable {
 	 *         All other exceptions will be treated as unexpected errors and will be displayed with error code 0.
 	 */
 	public function serve( \ArrayAccess $get, \ArrayAccess $post ) {
-		echo $this->key->get_product()->get_changelog();
+
+		$product = itelic_get_product( $get['ID'] );
+
+		if ( ! $product ) {
+			echo __( "No product found.", Plugin::SLUG );
+		} else {
+			echo $product->get_changelog();
+		}
 
 		die();
-	}
-
-	/**
-	 * Retrieve the mode of authentication.
-	 *
-	 * @since 1.0
-	 *
-	 * @return string One of MODE_ACTIVE, MODE_EXISTS
-	 */
-	public function get_auth_mode() {
-		return Authenticatable::MODE_EXISTS;
-	}
-
-	/**
-	 * Get the error message to be displayed if authentication is not provided.
-	 *
-	 * @since 1.0
-	 *
-	 * @return string
-	 */
-	public function get_auth_error_message() {
-		return __( "Invalid license key.", Plugin::SLUG );
-	}
-
-	/**
-	 * Get the error code to be displayed if authentication is not provided.
-	 *
-	 * @since 1.0
-	 *
-	 * @return int
-	 */
-	public function get_auth_error_code() {
-		return self::CODE_INVALID_KEY;
-	}
-
-	/**
-	 * Give a reference of the API key to this object.
-	 *
-	 * @since 1.0
-	 *
-	 * @param Key $key
-	 */
-	public function set_auth_license_key( Key $key ) {
-		$this->key = $key;
-	}
-
-	/**
-	 * Give a reference of the activation record to this object.
-	 *
-	 * @since 1.0
-	 *
-	 * @param Activation $activation
-	 */
-	public function set_auth_activation( Activation $activation = null ) {
-
 	}
 }
